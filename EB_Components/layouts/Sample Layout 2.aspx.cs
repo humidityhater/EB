01c38d1d1e3413096f036bb663666e0abb4bf014
Sean Grigg <Sean Grigg@WIN-NN9L64D0VM2>
2016-10-25 13:16:13 -0500

    Add project files.

---
+++ EB_Components/layouts/Sample Layout 2.aspx.cs
@@ -0,0 +1,13 @@
+namespace EB_Components.layouts
+{
+    using System;
+    using System.Web;
+    using System.Web.UI;
+
+    public partial class Sample_Layout_2 : Page
+    {
+        private void Page_Load(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
